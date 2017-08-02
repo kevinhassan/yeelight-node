838794759363cff12fb7811d0a93b0c897e3d771
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-08-02 23:48:54 +0200

    move test content to index server

--- index.js
+++ index.js
@@ -1,10 +1,72 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const logger  = require('morgan')
+const logger  = require('morgan');
+
+let schedule = require('node-schedule');
+
+let request = require('request');
+let ville = 'Aubagne'; // TODO: récupérer l'adresse ip et extraire la ville
+let weatherApi = 'http://api.openweathermap.org/data/2.5/weather';
+let apiKey = 'd0c96820636fedb6a4d8f08144cad566';
+let Sunset = require('./models/Sunset');
+let discover = require('./yeelight/discover');
+const duration = 60* 1000; //milliseconds
+
+
 const app = express();
 require('dotenv').config();
 
-if(process.env.NODE_ENV === 'development') app.use(logger('dev'))
+if(process.env.NODE_ENV === 'development') app.use(logger('dev'));
+
+checkSunset();
+
+//add schedule job for 0am to fetch automatically
+schedule.scheduleJob({hour: 0, minute: 0, second: 0}, function(){
+    checkSunset();
+});
+
+//TODO: catch errors
+function checkSunset(){
+    Sunset.findOne({'date': {$gte: new Date()}}, (err, result)=>{
+        // sunset time is stored in DB
+        if(result){
+            let delay = (result.date - new Date);//milliseconds
+            setTimeout(bulbPowerOn, delay);
+        }else{
+            //get sunset time and it to DB
+            getSunset().then((result)=>{
+                let delay = (result.date - new Date);//milliseconds
+                setTimeout(bulbPowerOn, delay);
+            }).catch((err)=>{
+                console.error(err);
+            });
+            //bulbPowerOn
+        }
+    });
+}
+function getSunset(){
+    return new Promise((resolve, reject)=>{
+        request(weatherApi+'?q='+ville+'&APPID='+apiKey, (err, res, body) => {
+            if(err) reject(new Error('Fetch weather api failed'));
+            let sunsetTime = new Date(JSON.parse(body).sys.sunset *1000);
+            if(new Date < sunsetTime){
+                let sunset = new Sunset({date: sunsetTime}).save((err)=>{
+                    if(err) console.error(err);
+                });
+                resolve(sunset);
+            }else{
+                reject(new Error('Cannot save anterior sunset time'));
+            }
+        });
+    });
+}
+function bulbPowerOn(){
+    discover().then((yeelights)=>{
+        yeelights.map((yeelight)=>{
+            yeelight.set_power('on', 'smooth', duration);
+        });
+    });
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
