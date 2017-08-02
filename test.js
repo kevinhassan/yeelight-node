fd5db06400ed948decf63552c73e5aa4a1b1d84c
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-08-02 15:14:50 +0200

    add: weather api integration for sunset time

--- test.js
+++ test.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 const logger  = require('morgan');
 let schedule = require('node-schedule');
 
+let request = require('request');
+let ville = 'Aubagne'; // récupérer l'adresse ip et extraire la ville
+let weatherApi = 'http://api.openweathermap.org/data/2.5/weather';
+let apiKey = 'd0c96820636fedb6a4d8f08144cad566';
+
 const app = express();
 require('dotenv').config();
 
@@ -19,6 +24,14 @@ schedule.scheduleJob(rule, function(){
 
 });
 
+function getSunset(){
+    return new Promise((resolve, reject)=>{
+        request(weatherApi+'?q='+ville+'&APPID='+apiKey, (err, res, body) => {
+            let sunset = new Date(JSON.parse(body).sys.sunset *1000);
+            return resolve(sunset);
+        });
+    });
+}
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), '0.0.0.0', () => {
